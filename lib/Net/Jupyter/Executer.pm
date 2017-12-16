8e185505ba323bd773915b8a64460bae52229767
gabrielash <gash234@yahoo.com>
2017-12-16 16:05:53 +0100

    0.1.2

--- lib/Net/Jupyter/Executer.pm
+++ lib/Net/Jupyter/Executer.pm
@@ -24,7 +24,8 @@ sub run-with-timeout(&start,  Int :$timeout where {$timeout >= 0 } = 0 ) is expo
                     Promise.in($timeout)\
                         .then({ #$*so.print("KILLING\n");
                                 $p.kill;
-                                sleep 2; $p.kill(9) }))
+                                sleep 2;
+                                $p.kill(9) }))
     if $timeout > 0;
   await $r;
 
@@ -73,9 +74,13 @@ class Executer is export {
       %!user-expressions = Hash.new unless %!user-expressions.defined;
       @!payload = [] unless @!payload.defined;
       %!metadata = Hash.new unless %!metadata.defined;
+
       $magic = Magic.parse( $!code );
       $!code = $magic.perl-code;
-
+      with $magic.classname {
+        $!code = remove-class-code($_) ~ "\nclass $_  \n$!code"
+          if $magic.class-status == single;
+      }
       self!run-code;
 
       self!run-expressions
